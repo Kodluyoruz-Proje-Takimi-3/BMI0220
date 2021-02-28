5250e645d19d904de9d5b3020dc10b6f0f5b6b23
Yağnur Tetikcan <50737107+yagnurl@users.noreply.github.com>
2021-02-28 23:21:20 +0300

    Delete src/dummy-data directory

--- src/dummy-data/logo.js
+++
@@ -1,9 +0,0 @@
-import bootstrap from "../img/bootstrap.png";
-import css from "../img/css3.png";
-import chartjs from "../img/chartjs.png";
-import mongodb from "../img/mongodb.png";
-import react from "../img/react.png";
-
-const logos = [react, bootstrap, mongodb, css, chartjs];
-
-export default logos;
